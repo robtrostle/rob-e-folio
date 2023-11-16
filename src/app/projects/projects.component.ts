12e8d5b0132f30769f5077eed095fef89a57e2cb
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2023-11-16 10:15:16 -0500

    adding animation to projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,123 @@
 import { Component } from '@angular/core';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'backgroundColor': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('highlighted <=> normal', animate(800))
+  ]),trigger('wildState', [
+    state('normal', style({
+      'background-color': 'red',
+      transform: 'translateX(0) scale(1)'
+    })),
+    state('highlighted', style({
+      'backgroundColor': 'blue',
+      transform: 'translateX(100px) scale(1)'
+    })),
+    state('shrunken', style({
+      'backgroundColor': 'green',
+      transform: 'translateX(0) scale(0.5)'
+    })),
+    transition('normal => highlighted', animate(300)),
+    transition('highlighted => normal', animate(800)),
+    transition('shrunken <=> *', [
+      style({
+        'backgroundColor': 'orange'
+       }),
+      animate(1000, style({
+       borderRadius: '50px'
+       })),
+      animate(500)
+     //wildard transition from any state
+     ])
+  ]),
+  trigger('list1', [
+    state('in', style({
+      opacity: 1,
+      transform: 'translateX(0)'
+    })),
+    transition('void => *', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100px)'
+      }),
+      animate(300)
+    ]),//from void (nonexistant) to any state
+    transition('* <=> void', [
+      animate(300, style({
+        transform: 'translateX(100px)',
+        opacity: 0
+      }))
+    ] ),
+]),
+trigger('list2', [
+  state('in', style({
+    opacity: 1,
+    transform: 'translateX(0)'
+  })),
+  transition('void => *', [
+    animate(1000, keyframes([
+      style({
+        transform: 'translateX(-100px)',
+        opacity: 0,
+        offset: 0
+      }),
+      style({
+        transform: 'translateX(-50px)',
+        opacity: 0.5,
+        offset: 0.3
+      }),
+      style({
+        transform: 'translateX(-20px)',
+        opacity: 1,
+        offset: 0.8
+      }),
+      style({
+        transform: 'translateX(0px)',
+        opacity: 1,
+        offset: 1
+      })
+    ]))
+  ]),
+  transition('* <=> void', [
+    group([
+      animate(300, style({
+        color: 'red'
+      })),
+      animate(800, style({
+        transform: 'translateX(100px)',
+        opacity: 0
+      }))
+    ])
+  ]),
+]),
+ ]
 })
+
 export class ProjectsComponent {
+
+  state = 'normal';
+  wildState = 'normal';
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
 }
